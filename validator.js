79d1a970ec6907e772cdc423332833311fa662e5
Kuntimaddi Manideep <manideepk90@gmail.com>
2023-12-28 12:13:45 +0530

    Update validator.js

--- validator.js
+++ validator.js
@@ -1,109 +1,138 @@
+
 export class Validator {
   constructor() {
     this.schema = {};
   }
-  string() {
+  string(customTypeMsg) {
     this.schema = {
       ...this.schema,
       type: "string",
+      customTypeMsg,
     };
     return this;
   }
-  number() {
+  number(customTypeMsg) {
     this.schema = {
       ...this.schema,
       type: "number",
+      customTypeMsg,
     };
     return this;
   }
-  boolean() {
+  boolean(customTypeMsg) {
     this.schema = {
       ...this.schema,
       type: "boolean",
+      customTypeMsg,
     };
     return this;
   }
-  Object() {
+  Object(customTypeMsg) {
     this.schema = {
       ...this.schema,
       type: "array",
+      customTypeMsg,
     };
     return this;
   }
-  isNullable() {
+  isNullable(nullableMsg) {
     this.schema = {
       ...this.schema,
       nullable: true,
+      nullableMsg,
     };
     return this;
   }
-  validateFunction(fn) {
+  validateFunction(fn, fnErrorMessage) {
     this.schema = {
       ...this.schema,
       validationFunction: fn,
+      fnErrorMessage,
     };
     return this;
   }
-  required() {
+  required(errorMessage) {
     this.schema = {
       ...this.schema,
       isRequired: true,
+      requiredErrorMessage: errorMessage,
     };
     return this;
   }
 }
 
 export class Validation {
-  constructor(schema, data) {
-    var errors = [];
+  constructor(schema, data, fn) {
+    this.errors = {};
     if (Object.keys(schema).length === 0) {
-      errors.push(new Error("No schema found"));
+      this.errors["main"] = new Error("No schema found");
     }
     this.schema = schema;
     this.data = data;
     for (const key of Object.keys(schema)) {
-      const isNullable = this.#isNullable(key);
-      if (!isNullable && this.#isNull(key)) {
-        errors.push(new Error(`${key} cannot be nullable`));
+      if (fn) {
+        fn(this, key);
+      } else {
+        this.checkData(this.schema[key], this.data[key], key);
       }
-      if (!this.#isString(key)) errors.push(new Error(`${key} must be string`));
-      if (!this.#isNumber(key)) errors.push(Error(`${key} must be an integer`));
-      if (this.#isRequired(key))
-        if (!this.#checkValues(key)) errors.push(Error(`${key} is required`));
     }
-    if (errors.length !== 0) {
-      return errors;
+    if (Object.keys(this.errors).length !== 0) {
+      return { errors: this.errors, data: {} };
     }
-    return data;
+    return { data: this.data, errors: {} };
   }
-  #isNullable(key) {
-    return this.schema[key].schema?.nullable
-      ? this.schema[key].schema?.nullable
-      : false;
+  checkData(schema, data, key) {
+    schema = schema?.schema;
+    const isNullable = this.#isNullable(schema);
+    if (!isNullable && this.#isNull(schema, data)) {
+      this.errors[key] = new Error(
+        schema?.nullableMsg ? schema?.nullableMsg : `${key} cannot be nullable`
+      );
+    }
+    if (!this.#isString(schema, data))
+      this.errors[key] = new Error(
+        schema?.customTypeMsg ? schema?.customTypeMsg : `${key} must be string`
+      );
+
+    if (!this.#isNumber(schema, data))
+      this.errors[key] = new Error(
+        schema?.customTypeMsg
+          ? schema?.customTypeMsg
+          : `${key} must be an integer`
+      );
+
+    if (this.#isRequired(schema, data))
+      if (!this.#checkValues(schema, data))
+        this.errors[key] = new Error(
+          schema?.requiredErrorMessage
+            ? schema?.requiredErrorMessage
+            : `${key} is required`
+        );
   }
-  #isNull(key) {
-    return this.data[key] === undefined || this.data[key] === null;
+  #isNullable(schema) {
+    return schema?.nullable ? schema?.nullable : false;
   }
-  #isString(key) {
-    if (this.#isNull(key)) {
+  #isNull(schema, data) {
+    return data === undefined || data === null;
+  }
+  #isString(schema, data) {
+    if (this.#isNull(schema, data)) {
       return true;
     }
-    if (this.schema[key].schema?.type === "string")
-      return typeof this.data[key] === "string";
+    if (schema?.type === "string") return typeof data === "string";
     return true;
   }
-  #isNumber(key) {
-    if (this.#isNull(key)) {
+  #isNumber(schema, data) {
+    if (this.#isNull(schema, data)) {
       return true;
     }
-    if (this.schema[key].schema?.type === "number")
-      return typeof this.data[key] === "number";
+    if (schema?.type === "number") return typeof data === "number";
     return true;
   }
-  #checkValues(key) {
-    return this.data[key] ? true : false;
+  #checkValues(schema, data) {
+    return data ? true : false;
   }
-  #isRequired(key) {
-    return this.schema[key].schema?.isRequired ? true : false;
+  #isRequired(schema, data) {
+    return schema?.isRequired ? true : false;
   }
 }
